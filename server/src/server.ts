f83be99c9d060f4d4a4c717c4770896f3c301096
Gustavo felipe <gustavo.felipebc@gmail.com>
2023-06-05 11:19:01 -0300

    aula 01

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  // console.log(users)
+  return users
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
---
+++ web/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 p-6">
+      <h1 className="font-sans text-4xl font-bold text-zinc-50">
+        Sua cápsula do tempo
+      </h1>
+      <h1 className="font-alt text-4xl font-bold text-zinc-50">
+        Sua cápsula do tempo
+      </h1>
+    </div>
+  )
+}
